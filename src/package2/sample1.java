269088365518cb35853f3d42538ddd5eb1e150e9
ashitha123456 <ashithashetty064@gmail.com>
2019-04-26 11:15:47 +0530

    second

--- src/package2/sample1.java
+++ src/package2/sample1.java
@@ -5,20 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class sample1 {
-	
+public class sample1{
 	@Test
 	 public void run()
 	 {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Desktop\\newselenium\\Simplemaven\\chrome\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://docs.seleniumhq.org/");
-		System.out.println(driver.getTitle());
+		driver.get("https://www.seleniumhq.org/");
 		driver.manage().window().maximize();
 		driver. findElement(By.xpath("//a[text()='Download']")).click();
+		
+		System.out.println(driver.getTitle());
+		System.out.println("selenium");
+		driver.findElement(By.xpath("//a[text()='Documentation']")).click();
 		System.out.println(driver.getTitle());
 		driver.close();
 		
 		
 	 }
+	
+
 }
